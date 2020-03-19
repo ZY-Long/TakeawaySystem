a9fd13ff52b15feafef0d88fb5e41be3c8121ad9
jianghan134 <62366052+jianghan134@users.noreply.github.com>
2020-03-19 19:12:15 +0800

    Response

--- Takeaway_Api/SDK/TakeawayApi/TakeOutResponse/Take/TakeawayDeleteResponse.cs
+++ Takeaway_Api/SDK/TakeawayApi/TakeOutResponse/Take/TakeawayDeleteResponse.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SDK
+namespace SDK.TakeawayApi.TakeOutResponse.Take
 {
-    public class Class1
+    public class TakeawayDeleteResponse: TakeawayResponse
     {
     }
 }
