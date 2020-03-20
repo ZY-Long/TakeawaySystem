991641dc6899ba4ea4c19d952b82ed501fa0b6db
TR <TR@DESKTOP-3LFUUQC>
2020-03-20 12:10:11 +0800

    w

---
+++ Takeaway_Api/DAL/DataReaderHelper/DataReaderHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace DAL
+{
+   public static class DataReaderHelper
+    {
+        public static List<T> DataReaderToList<T>(this SqlDataReader reader)where T:new()
+        {
+            List<T> list = new List<T>();
+            while (reader.Read())
+            {
+                T t = new T();
+                var pops = t.GetType().GetProperties();
+                foreach (var pop in pops)
+                {
+                    pop.SetValue(t,reader[pop.Name]);
+                }
+                list.Add(t);
+            }
+            return list;
+        }
+    }
+}
