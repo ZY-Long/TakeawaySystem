cc73fde9c5b32f6ae3b506656ca94485ada6e859
Shuaihang666 <62366682+Shuaihang666@users.noreply.github.com>
2020-03-24 11:58:57 +0800

    合并项目

---
+++ Takeaway_Api/SDK/TakeawayApi/TakeawayResponse/地区相关/CityResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace SDK
+{
+    /// <summary>
+    /// 城市相关返回
+    /// </summary>
+    public class CityResponse:BaseResponse
+    {
+        public List<CityInfo> CityInfos { get; set; }
+    }
+}
---
+++ Takeaway_Api/SDK/TakeawayApi/TakeawayResponse/地区相关/ProvinceResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace SDK
+{
+    /// <summary>
+    /// 省份相关返回
+    /// </summary>
+    public class ProvinceResponse:BaseResponse
+    {
+        public List<ProvinceInfo>  ProvinceInfos { get; set; }
+    }
+}
