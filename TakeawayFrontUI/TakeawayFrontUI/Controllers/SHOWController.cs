89386c9e860b3e7f20170e6bf3e4f7c1d287e5dc
jianghan134 <62366052+jianghan134@users.noreply.github.com>
2020-03-26 13:40:26 +0800

    显示试

---
+++ TakeawayFrontUI/TakeawayFrontUI/Controllers/SHOWController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using SDK;
+
+namespace TakeawayFrontUI.Controllers
+{
+    public class SHOWController : Controller
+    {
+        ShowBll bll = new ShowBll();
+        // GET: SHOW
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult show(ShowRequest showRequest)
+        {
+            return Json(bll.Show(showRequest));
+        }
+    }
+}
