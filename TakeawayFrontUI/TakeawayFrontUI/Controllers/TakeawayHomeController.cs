f81d83851d458b838b5e0e49a0a92b19889bbf38
彦隆 张 <3086362103@qq.com>
2020-03-26 13:58:58 +0800

    页面

---
+++ TakeawayFrontUI/TakeawayFrontUI/Controllers/TakeawayHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TakeawayFrontUI.Controllers
+{
+    public class TakeawayHomeController : Controller
+    {
+        // GET: TakeawayHome
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
