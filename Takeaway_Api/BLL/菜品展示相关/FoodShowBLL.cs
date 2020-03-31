cc8b89a56877e24e25fc2c1d594eef9ead2bb111
TR <TR@DESKTOP-3LFUUQC>
2020-03-31 21:50:16 +0800

    ..

--- Takeaway_Api/BLL/菜品展示相关/FoodShowBLL.cs
+++ Takeaway_Api/BLL/菜品展示相关/FoodShowBLL.cs
@@ -11,9 +11,9 @@ namespace BLL
 {
     public class FoodShowBLL
     {
-        public show Show(int currPage, string Name, int TypeId)
+        public show Show(int currPage,  int TypeId, string Name)
         {
-            return BaseDAL<FoodShowDAL>.Instance.Show(currPage,Name,TypeId);
+            return BaseDAL<FoodShowDAL>.Instance.Show(currPage, TypeId, Name);
         }
     }
 }
