45b82714711bd306ccd0502848acb840c2a5dc3d
TR <TR@DESKTOP-3LFUUQC>
2020-03-31 19:33:38 +0800

    ..

--- Takeaway_Api/Model/TakeOut/购物车相关/CartInfos.cs
+++ Takeaway_Api/Model/TakeOut/购物车相关/CartInfos.cs
@@ -8,10 +8,8 @@ namespace Model
 {
    public class CartInfos
     {
-        public int Id { get; set; }
         public string Img { get; set; }
         public string Name { get; set; }
-        public string Remark { get; set; }
         public decimal Price { get; set; }
         public int Count { get; set; }
         public decimal ToPrice { get; set; }
