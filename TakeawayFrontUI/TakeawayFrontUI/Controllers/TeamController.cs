ed7eb783c1859ae248b7162225a3fd17cbeae726
jianghan134 <62366052+jianghan134@users.noreply.github.com>
2020-04-08 14:33:31 +0800

    提交

---
+++ TakeawayFrontUI/TakeawayFrontUI/Controllers/TeamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TakeawayFrontUI.Controllers
+{
+    public class TeamController : Controller
+    {
+        
+        // GET: Team
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MEMBER()
+        {
+            return View();
+        }
+    }
+}
